6e48696b6c32554bdb1228a36a6c35838e88380e
Swapna Avula <savula@planittesting.com>
2024-05-29 11:20:18 +0100

    cucumber practice1

--- src/test/steps/storesearchstep.ts
+++ src/test/steps/storesearchstep.ts
@@ -1,12 +1,5 @@
 import { Given, When, Then, setDefaultTimeout } from '@cucumber/cucumber';
 import {chromium, Page, Browser, defineConfig} from "@playwright/test";
-/*export default defineConfig({
-   use: {
-            extraHTTPHeaders: {
-        'X-Security-Header': 'barrel-expert'
-
-       }}})
- */
    let browser: Browser;
 let page: Page;
 setDefaultTimeout(60 * 1000 * 2);
@@ -14,33 +7,23 @@ setDefaultTimeout(60 * 1000 * 2);
 Given('user navigates to specsaver home page', async function () {
  browser = await chromium.launch({headless: false, });
   let context = await browser.newContext({ extraHTTPHeaders: { 'X-Security-Header': 'barrel-expert', },})
-    page = await context.newPage();
-    //await context.grantPermissions(['geolocation'], { origin: 'https://qa-gb-luc-main.az.ssdgws.co.uk/stores' })
+  page = await context.newPage();
   await page.goto("https://qa-gb-luc-main.az.ssdgws.co.uk/");
-  await page.locator('xpath=//*[@id="header"]/div[2]/div[1]/div/div/div[1]/ul/li[1]/a').click()
-  await context.grantPermissions(['geolocation'], { origin: 'https://qa-gb-luc-main.az.ssdgws.co.uk/stores' })
   await console.log("launched homepage")
    
-    
-  });
-
+     });
 
   When('user click on Find a store link', async function () {
-   // await page.locator('xpath=//*[@id="header"]/div[2]/div[1]/div/div/div[1]/ul/li[1]/a').click()
-    
-        await console.log("clickon find a store")
-        
-
-                //await page.pause()
-        
+   await page.locator('xpath=//*[@id="header"]/div[2]/div[1]/div/div/div[1]/ul/li[1]/a').click()
+            await console.log("clickon find a store")
+       //I need a code to click allow button on geolocation popup 
+       //await context.grantPermissions(['geolocation'], { origin: 'https://qa-gb-luc-main.az.ssdgws.co.uk/stores' })
   });
 
   When('user key the Coventry store and hit enter', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    await console.log("type coventry")
+        await console.log("type coventry")
   });
 
   Then('user see the Coventry store card', async function () {
-    // Write code here that turns the phrase above into concrete actions
-    await console.log("see results for coventry")
+        await console.log("see results for coventry")
   });
